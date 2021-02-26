4d696dde6a690ff0de39f6b809bd599ac7ebd18a
KKofta <63394533+KKofta@users.noreply.github.com>
2021-02-26 11:32:04 +0100

    Added GUI graphical fixes

--- TicTacToe/src/tictactoe/gui/GUICreation.java
+++ TicTacToe/src/tictactoe/gui/GUICreation.java
@@ -5,9 +5,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -31,7 +33,7 @@ public class GUICreation extends Application {
         VBox menu = createMenu();
         layout.setRight(menu);
 
-        Scene scene = new Scene(layout, 600, 600);
+        Scene scene = new Scene(layout, 650, 600);
         scene.getStylesheets().add(getClass().getResource("Styles.css").toExternalForm());
 
         stage.setTitle("TicTacToe with AI");
@@ -75,17 +77,36 @@ public class GUICreation extends Application {
     }
 
     private VBox createMenu() {
-        VBox menu = new VBox(40);
+        VBox menu = new VBox(20);
         menu.setAlignment(Pos.CENTER);
         menu.setPadding(new Insets(10, 15, 15, 15));
+        
+        Button infoButton = new Button("Information");
 
         Button aloneButton = new Button("Play alone");
-        Button randomButton = new Button("Play with random");
-        Button aiButton = new Button("Play and lose");
+        menu.setMargin(aloneButton, new Insets(100, 0, 0, 0));
+        aloneButton.setId("alone-button");
+        
+        Label randomLabel = new Label("Play with random:");
+        Button randomFirstButton = new Button("Go first");
+        randomFirstButton.setId("randfirst-button");
+        Button randomSecondButton = new Button("Go second");
+        randomSecondButton.setId("randsecond-button");
+        HBox randomBox = new HBox(10);
+        randomBox.getChildren().addAll(randomFirstButton, randomSecondButton);
+        
+        Label aiLabel = new Label("Play with AI:");
+        Button aiFirstButton = new Button("Go first");
+        aiFirstButton.setId("aifirst-button");
+        Button aiSecondButton = new Button("Go second");
+        aiSecondButton.setId("aisecond-button");
+        HBox aiBox = new HBox(10);
+        aiBox.getChildren().addAll(aiFirstButton, aiSecondButton);
+        
         Button exitButton = new Button("Exit");
 
-        menu.setMargin(exitButton, new Insets(200, 0, 0, 0));
-        menu.getChildren().addAll(aloneButton, randomButton, aiButton, exitButton);
+        menu.setMargin(exitButton, new Insets(100, 0, 0, 0));
+        menu.getChildren().addAll(infoButton, aloneButton, randomLabel, randomBox , aiLabel, aiBox ,exitButton);
 
         return menu;
     }
